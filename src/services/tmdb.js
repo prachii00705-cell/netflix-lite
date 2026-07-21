a7dbe168f60d82b42c90a7be536c88c38f3578f6
unknown <prachii00705@gmail.com>
2026-07-22 00:00:53 +0530

    changes made in api

--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -2,28 +2,40 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-  headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-    "Content-Type": "application/json",
-  },
 });
+
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
 
 export async function getPopularMovies(page = 1) {
   const response = await api.get(
-    `/movie/popular?page=${page}`
+    "/movie/popular",
+    {
+      params: {
+        api_key: API_KEY,
+        page,
+      },
+    }
   );
 
   return response.data;
 }
+
 
 export async function searchMovies(
   query,
   page = 1
 ) {
   const response = await api.get(
-    `/search/movie?query=${encodeURIComponent(
-      query
-    )}&page=${page}`
+    "/search/movie",
+    {
+      params: {
+        api_key: API_KEY,
+        query,
+        page,
+      },
+    }
   );
 
   return response.data;
